14d7f15c78064e041cc39579c00eeb559d101da0
hi <101079554+whyyt@users.noreply.github.com>
2026-04-04 03:33:23 +0800

    update

--- frontend/src/pages/CloudAdoption.tsx
+++ frontend/src/pages/CloudAdoption.tsx
@@ -1,10 +1,13 @@
 import { StandalonePageSection } from "../components/sections/StandalonePageSection";
 import { standalonePages } from "../data/site";
+import { useLocation } from "react-router-dom";
 
 export default function CloudAdoption() {
+  const { search } = useLocation();
+
   return (
     <StandalonePageSection
-      src={standalonePages.cloudAdoption.src}
+      src={`${standalonePages.cloudAdoption.src}${search}`}
       title={standalonePages.cloudAdoption.title}
     />
   );
